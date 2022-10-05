d5cad852e12cb37d29bc32bcb61c6f3704472b02
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-05 16:35:21 -0300

    operadores ternarios 2

---
+++ operadores-java/src/Ternario2.java
@@ -0,0 +1,19 @@
+public class Ternario2 {
+    public static void main(String[] args) {
+        int numero1 = 2;
+        int numero2 = 1;
+
+        if (numero1 > numero2)
+            System.out.println("Numero 1 maior que numero 2");
+
+        else if (numero1 < numero2)
+            System.out.println("Numero 1 menor que numero 2");
+
+        else
+            System.out.println("Os numeros sao iguais");
+
+        // >= MAIOR OU IGUAL
+        // <= MENOR OU IGUAL
+        // != DIFERENTE DE
+    }
+}
