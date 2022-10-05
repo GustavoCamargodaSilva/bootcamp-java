fd92aa6d64eddf0adc30c5a6c400bc6a91b57722
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-05 15:54:46 -0300

    concatenação strings

---
+++ operadores-java/src/concatenação.java
@@ -0,0 +1,9 @@
+public class concatenação {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + " JAVA";
+        System.out.println(nomeCompleto);
+
+        // CONCATENAÇÃO DE STRINGS
+        // CONCATENAÇÃO DE STRINGS.
+    }
+}
