402d725d1ad62f13e3f1dbcb42967da834d58539
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-05 16:05:24 -0300

    operador ternario resumido

---
+++ operadores-java/src/Ternario.java
@@ -0,0 +1,22 @@
+public class Ternario {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        // String resultado = "";
+
+        // if (a == b)
+        // resultado = "verdadeiro";
+        // else
+        // resultado = "falso";
+
+        // System.out.println(resultado);
+
+        // forma abreviada
+
+        String resultado = a == b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
+    }
+}
