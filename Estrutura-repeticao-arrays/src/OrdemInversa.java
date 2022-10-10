3d50aac9bfe5ac2d88d388a02f7192634c033b2e
Gustavo Camargo <gustavocamargodasilva@hotmail.com>
2022-10-10 11:50:36 -0300

    Array laço repetição ordem OrdemInversa

---
+++ Estrutura-repeticao-arrays/src/OrdemInversa.java
@@ -0,0 +1,15 @@
+public class OrdemInversa {
+    public static void main(String[] args) {
+
+        int[] vetor = { -5, -6, 15, 50, 8, 4 };
+        int count = 0;
+        while (count < (vetor.length)) {
+            // System.out.print(vetor[count] + " ");
+            count++;
+        }
+
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
